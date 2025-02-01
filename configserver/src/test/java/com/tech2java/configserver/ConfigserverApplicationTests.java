155a9abcc1d9f3c4ebb6ae36f174b220d3bdd5fd
tech2java <162876323+tech2java@users.noreply.github.com>
2025-02-01 23:59:40 +0530

    config server
    config server

---
+++ configserver/src/test/java/com/tech2java/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech2java.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
